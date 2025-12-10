9621fb3d3c2f1e5a4a1b18479e814af9699b5c4e
Wineshuga <uzochukwuwinnie@gmail.com>
2025-12-11 00:21:12 +0100

    Move router folder out of app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.routers import trips, users
+from routers import trips, users
 from .database import create_db_and_tables
 
 @asynccontextmanager
