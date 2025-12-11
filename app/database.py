fe212aff6ddb5245648032ac1a1eee8fd8f10d23
Wineshuga <uzochukwuwinnie@gmail.com>
2025-12-12 00:00:14 +0100

    Create a global get_session function

--- app/database.py
+++ app/database.py
@@ -12,3 +12,7 @@ async_session = sessionmaker(
 async def create_db_and_tables():
   async with engine.begin() as conn:
     await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+  async with async_session() as session:
+    yield session
