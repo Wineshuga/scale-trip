57a95066bc469b5e8a27f885225785f0cd1e11c9
Wineshuga <uzochukwuwinnie@gmail.com>
2025-12-15 23:52:04 +0100

    Create helper test func for create expenses

--- tests/utils.py
+++ tests/utils.py
@@ -16,3 +16,26 @@ async def create_user_and_trip(client):
     trip_resp = await client.post("/trips/", json=trip_payload)
     trip = trip_resp.json()["result"][0]
     return user, trip
+
+async def create_expenses(client):
+    _, created_trip = await create_user_and_trip(client)
+
+    payload1 = {
+        "trip_id": created_trip["id"],
+        "description": "describing",
+        "amount": 1000,
+        "date": "2025-12-11T01:42:23.603586",
+        "note": "note",
+    }
+    payload2 = {
+        "trip_id": created_trip["id"],
+        "description": "describing more",
+        "amount": 2000,
+        "date": "2025-12-11T01:42:23.603586",
+        "note": "note",
+    }
+    resp1 = await client.post("/expenses/", json=payload1)
+    resp2 = await client.post("/expenses/", json=payload2)
+
+    return created_trip
+
