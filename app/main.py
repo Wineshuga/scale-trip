378ee3c9517e4b3bd5a572b4799f1156327cd1fc
Wineshuga <uzochukwuwinnie@gmail.com>
2025-12-17 13:48:38 +0100

    Import and use OAuth2PasswordBearer

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
 from routers import trips, users , expenses
 from .database import create_db_and_tables
+from fastapi.security import OAuth2PasswordBearer
 
 @asynccontextmanager
 async def startup(app: FastAPI):
@@ -10,9 +11,11 @@ async def startup(app: FastAPI):
 
 app = FastAPI(lifespan=startup)
 
-app.include_router(users.router)
-app.include_router(trips.router)
-app.include_router(expenses.router)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app.include_router(users.router, dependencies=[Depends(oauth2_scheme)])
+app.include_router(trips.router, dependencies=[Depends(oauth2_scheme)])
+app.include_router(expenses.router, dependencies=[Depends(oauth2_scheme)])
 
 @app.get("/")
 async def root():
